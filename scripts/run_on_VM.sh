6a737a623dd21670171f07cb3e624aa32b878b7f
Jakub Fabo <fabojakub9@gmail.com>
2022-06-21 21:07:45 +0200

    Made scripts portable

--- scripts/run_on_VM.sh
+++ scripts/run_on_VM.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
+
+## resolve folder of this script, following all symlinks,
+## http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SCRIPT_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
+  SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$SCRIPT_DIR/$SCRIPT_SOURCE"
+done
+readonly SCRIPT_ORIGIN="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
+readonly REPO_DIR=`dirname $SCRIPT_ORIGIN`
+readonly REPO_NAME=`basename $REPO_DIR`
+
 set -exo pipefail
 
-export WORKSPACE=`mktemp -d`
-mkdir  $WORKSPACE/in
-mkdir  $WORKSPACE/out
+if [ "x$WORKSPACE" = "x" ] ; then 
+  isTmpWs="true"
+  export WORKSPACE=`mktemp -d`
+  mkdir  $WORKSPACE/in
+  mkdir  $WORKSPACE/out
+fi
 
-SCRIPT_ORIGIN=/home/tester/diplomka/benchmarks-in-nested-virtualisation-toolchain/scripts
 VIRTUAL_WORKSPACE=/mnt/workspace
-cp $SCRIPT_ORIGIN/script.sh  $WORKSPACE/in
-pushd /home/tester/diplomka/nested/f34-x64
+pushd $SCRIPT_ORIGIN/../vagrantfiles/nested/f34-x64
   vagrant destroy -f
   vagrant up
-  vagrant ssh -c "bash $VIRTUAL_WORKSPACE/in/script.sh"
+  vagrant ssh -c "bash $VIRTUAL_WORKSPACE/in/$REPO_NAME/scripts/script.sh"
 popd
 
 find $WORKSPACE
-cp -r $WORKSPACE/out  $VIRTUAL_WORKSPACE
+if [ "x$isTmpWs" = "xtrue" ] ; then 
+  cp -r $WORKSPACE/out  $VIRTUAL_WORKSPACE
+fi
 
 
