7ac53abd52ab91c296cdf00be8902f10760ca88a
Jakub Fabo <fabojakub9@gmail.com>
2022-06-22 13:53:44 +0200

    Added JDKiterator and modified other scripts and config accordingly

--- scripts/jdkiterator.sh
+++ scripts/jdkiterator.sh
@@ -10,38 +10,18 @@ while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer 
   [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$SCRIPT_DIR/$SCRIPT_SOURCE"
 done
 readonly SCRIPT_ORIGIN="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
-readonly REPO_DIR=`dirname $SCRIPT_ORIGIN`
+	readonly REPO_DIR=`dirname $SCRIPT_ORIGIN`
 readonly REPO_NAME=`basename $REPO_DIR`
 
 set -exo pipefail
 
-JDK=$2
-COUNTER=$1
-if [ ! "x$DEV" = "x" ] ; then
-  export VM_CPUS=2
-  export VM_MEMORY=2048
-fi
+JDK_DIR=$(cat $SCRIPT_ORIGIN/../config | grep JDK_DIR | sed "s/.*=//")
+ITER_NUM=$(cat $SCRIPT_ORIGIN/../config | grep ITER_NUM | sed "s/.*=//")
 
-if [ "x$WORKSPACE" = "x" ] ; then 
-  isTmpWs="true"
-  export WORKSPACE=`mktemp -d`
-  mkdir  $WORKSPACE/in
-  mkdir  $WORKSPACE/out
-  cp -r $REPO_DIR $WORKSPACE/in
-  cp $JDK $WORKSPACE/in
-fi
-
-VIRTUAL_WORKSPACE=/mnt/workspace
-pushd $SCRIPT_ORIGIN/../vagrantfiles/nested/$(cat $SCRIPT_ORIGIN/../config | grep NESTED | sed "s/.*=//")
-  vagrant destroy -f
-  vagrant up
-  vagrant ssh -c "bash $VIRTUAL_WORKSPACE/in/$REPO_NAME/scripts/script.sh"
-popd
-
-find $WORKSPACE
-if [ "x$isTmpWs" = "xtrue" ] ; then 
-  cp -r $WORKSPACE/out  $SCRIPT_ORIGIN/../results
-  rm -rfv $WORKSPACE
-fi
-
-
+JDKS=`find  $JDK_DIR -type f`
+for jdk in $JDKS ; do
+  for x in `seq 1 $ITER_NUM` ; do
+    # DEV=halt sh $SCRIPT_ORIGIN/run_on_VM.sh $x `readlink -f $jdk`
+    DEV=true sh $SCRIPT_ORIGIN/run_on_nested_VM $x `readlink -f $jdk`
+  done
+done
