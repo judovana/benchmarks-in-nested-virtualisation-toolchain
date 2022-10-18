2faec89b6d23cd18468c209ed934c279342a534c
Jakub Fabo <fabojakub9@gmail.com>
2022-10-18 14:18:51 +0200

    Unpacking JDK in home instead of workspace, because tar struggled for some reason and produced cryptic messages.

--- scripts/script.sh
+++ scripts/script.sh
@@ -9,12 +9,18 @@ uname -a > $RESULT_DIR/uname_output.txt
 cat /proc/cpuinfo > $RESULT_DIR/cpuinfo_output.txt
 cat /proc/meminfo > $RESULT_DIR/meminfo_output.txt
 cat /etc/redhat-release > $RESULT_DIR/redhat_release_output.txt
+
 JDK=`basename $1`
-rm -rf jdk
-mkdir jdk
-pushd jdk
+JDK_UNPACKED="$HOME/jdk"
+rm -rvf "$JDK_UNPACKED"
+mkdir "$JDK_UNPACKED"
+pushd "$JDK_UNPACKED"
   tar -xf $WORKSPACE/in/$JDK --strip-components=1 
 popd
-./jdk/bin/java -version 
-./jdk/bin/java -version > $RESULT_DIR/javao
-./jdk/bin/java -version 2> $RESULT_DIR/javae
+ls -l "$JDK_UNPACKED" >> $RESULT_DIR/jdk.txt
+
+"$JDK_UNPACKED"/bin/java -version 
+"$JDK_UNPACKED"/bin/java -version > $RESULT_DIR/javao
+"$JDK_UNPACKED"/bin/java -version 2> $RESULT_DIR/javae
+
+
