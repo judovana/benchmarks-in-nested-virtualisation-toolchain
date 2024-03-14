3f292aa38124142cad101eadf19da099e466e841
Jakub Fabo <fabojakub9@gmail.com>
2024-03-14 14:55:52 +0100

    Repaired normal VM functionality that broke with time
    Normal VM was accidentally using some of the code for nested VM that caused it to not work and copy results.

--- scripts/run_on_VM.sh
+++ scripts/run_on_VM.sh
@@ -18,12 +18,14 @@ set -exo pipefail
 JDK=$2
 JDK_NAME=`basename ${JDK}`
 COUNTER=$1
+
+#DEV mode for dev machine with low HW specs
 if [ ! "x$DEV" = "x" ] ; then
   export VM_CPUS=2
   export VM_MEMORY=2048
 fi
 
-#i am not sure how this is supposed to work
+#create temporary workspace if necessary
 if [ "x$WORKSPACE" = "x" ] ; then 
   isTmpWs="true"
   export WORKSPACE=`mktemp -d`
@@ -33,12 +35,24 @@ if [ "x$WORKSPACE" = "x" ] ; then
   cp $JDK $WORKSPACE/in
 fi
 
+#create result folder structure
 mkdir $SCRIPT_ORIGIN/../results || true
 mkdir $SCRIPT_ORIGIN/../results/${JDK_NAME} || true
 mkdir $SCRIPT_ORIGIN/../results/${JDK_NAME}/${COUNTER}
 
+#check if the VM is running on real hardware host, or if the VM is nested
+RUN_TYPE=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep RUN_TYPE | sed "s/.*=//")
+if [ "x$RUN_TYPE" = "xVM" ] ; then 
+  LAYER=MAINVM
+  VAGRANTFILES_FOLDER=normal
+else
+  LAYER=NESTED
+  VAGRANTFILES_FOLDER=nested
+fi
+
+#start VM and run the selected benchmark
 VIRTUAL_WORKSPACE=/mnt/workspace
-pushd $SCRIPT_ORIGIN/../vagrantfiles/nested/$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep ^NESTED= | sed "s/.*=//")
+pushd $SCRIPT_ORIGIN/../vagrantfiles/$VAGRANTFILES_FOLDER/$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep ^$LAYER= | sed "s/.*=//")
   vagrant destroy -f
   vagrant up
   vagrant ssh -c "echo '127.0.0.1   localhost results' | sudo tee -a /etc/hosts "
@@ -50,17 +64,20 @@ popd
 find $WORKSPACE
 #find $VIRTUAL_WORKSPACE
 
-RUN_TYPE=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep RUN_TYPE | sed "s/.*=//")
-TOP_LEVEL_HOST=$(cat $SCRIPT_ORIGIN/../config | grep ^TOP_LEVEL_HOST= | sed "s/.*=//")
-MIDDLE_POINT=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep MIDDLE_POINT | sed "s/.*=//")
-
+#copy results from VM or nested VM to base host.
 if [ "x$isTmpWs" = "xtrue" ] || [ "x$RUN_TYPE" = "xnested_VM" ] ; then 
+  TOP_LEVEL_HOST=$(cat $SCRIPT_ORIGIN/../config | grep ^TOP_LEVEL_HOST= | sed "s/.*=//")
+  MIDDLE_POINT=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep MIDDLE_POINT | sed "s/.*=//")
   find $WORKSPACE/results/
   #mkdir -p home/tester/test/results/java-1.8.0-openjdk-jdk8u122.b01-0.ojdk8~u~upstream.hotspot.release.sdk.el7.x86_64.tarxz/1
   #rsync -av -e "ssh -o StrictHostKeyChecking=no" --relative --progress --exclude .git $WORKSPACE/results/ home/tester/test/results/${JDK_NAME}/${COUNTER}
   sudo dnf -y install /usr/bin/ssh
-  ssh tester@$TOP_LEVEL_HOST "mkdir -p $WORKSPACE/results/${JDK_NAME}/${COUNTER}"
-  rsync -av -e "ssh -o StrictHostKeyChecking=no"  --progress --exclude .git  $WORKSPACE/out  $WORKSPACE/results tester@$TOP_LEVEL_HOST:$WORKSPACE/results/${JDK_NAME}/${COUNTER}
+  if [ "x$RUN_TYPE" = "xVM" ] ; then 
+    rsync -av -e "ssh -o StrictHostKeyChecking=no"  --progress --exclude .git  $WORKSPACE/out  $WORKSPACE/results tester@$TOP_LEVEL_HOST:$SCRIPT_ORIGIN/../results/${JDK_NAME}/${COUNTER}
+  else
+    ssh tester@$TOP_LEVEL_HOST "mkdir -p $WORKSPACE/results/${JDK_NAME}/${COUNTER}"
+    rsync -av -e "ssh -o StrictHostKeyChecking=no"  --progress --exclude .git  $WORKSPACE/out  $WORKSPACE/results tester@$TOP_LEVEL_HOST:$WORKSPACE/results/${JDK_NAME}/${COUNTER}
+  fi
   rm -rfv $WORKSPACE
 fi
 
