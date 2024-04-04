1e99773a8925a3adecbbb6c3b248495986a5a4e4
Jakub Fabo <fabojakub9@gmail.com>
2024-04-04 18:39:15 +0200

    No longer using shared folder in nested VM, using rsync instead

--- scripts/run_on_VM.sh
+++ scripts/run_on_VM.sh
@@ -40,7 +40,7 @@ mkdir $SCRIPT_ORIGIN/../results || true
 mkdir $SCRIPT_ORIGIN/../results/${JDK_NAME} || true
 mkdir $SCRIPT_ORIGIN/../results/${JDK_NAME}/${COUNTER}
 
-#check if the VM is running on real hardware host, or if the VM is nested
+#check if the VM is supposed to run on real hardware host, or if the VM will be nested
 RUN_TYPE=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep RUN_TYPE | sed "s/.*=//")
 if [ "x$RUN_TYPE" = "xVM" ] ; then 
   LAYER=MAINVM
@@ -50,11 +50,18 @@ else
   VAGRANTFILES_FOLDER=nested
 fi
 
+TOP_LEVEL_HOST=$(cat $SCRIPT_ORIGIN/../config | grep ^TOP_LEVEL_HOST= | sed "s/.*=//")
+
 #start VM and run the selected benchmark
 VIRTUAL_WORKSPACE=/mnt/workspace
 pushd $SCRIPT_ORIGIN/../vagrantfiles/$VAGRANTFILES_FOLDER/$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep ^$LAYER= | sed "s/.*=//")
   vagrant destroy -f
   vagrant up
+  if [ "x$isTmpWs" = "xtrue" ] || [ "x$RUN_TYPE" = "xnested_VM" ] ; then
+    sudo dnf -y install /usr/bin/ssh rsync
+    vagrant ssh -c "sudo dnf -y install /usr/bin/ssh rsync"
+    vagrant ssh -c "rsync -av -e \"ssh -o StrictHostKeyChecking=no\"  --progress --exclude .git  tester@$TOP_LEVEL_HOST:/mnt/shared /mnt/shared"
+  fi
   vagrant ssh -c "echo '127.0.0.1   localhost results' | sudo tee -a /etc/hosts "
   vagrant ssh -c "echo '::1         localhost results' | sudo tee -a /etc/hosts "
   vagrant ssh -c "bash $VIRTUAL_WORKSPACE/in/$REPO_NAME/scripts/script.sh $JDK"
@@ -66,7 +73,6 @@ find $WORKSPACE
 
 #copy results from VM or nested VM to base host.
 if [ "x$isTmpWs" = "xtrue" ] || [ "x$RUN_TYPE" = "xnested_VM" ] ; then 
-  TOP_LEVEL_HOST=$(cat $SCRIPT_ORIGIN/../config | grep ^TOP_LEVEL_HOST= | sed "s/.*=//")
   MIDDLE_POINT=$(cat $SCRIPT_ORIGIN/../config | grep -v "^#" | grep MIDDLE_POINT | sed "s/.*=//")
   find $WORKSPACE/results/
   #mkdir -p home/tester/test/results/java-1.8.0-openjdk-jdk8u122.b01-0.ojdk8~u~upstream.hotspot.release.sdk.el7.x86_64.tarxz/1
